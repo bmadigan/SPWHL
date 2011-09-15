f4391cb905bfff4bf7dc8b16116cc8c1e568f33b
Brad Madigan <brad.madigan@dizzytree.com>
2011-09-15 15:13:24 -0400

    added fix for migration

--- db/migrate/20110711192214_remove_tournament_minor_tweaks_schedule.rb
+++ db/migrate/20110711192214_remove_tournament_minor_tweaks_schedule.rb
@@ -1,15 +1,15 @@
 class RemoveTournamentMinorTweaksSchedule < ActiveRecord::Migration
   def self.up
     #remove_column :schedules, :is_league_run
-    remove_column :schedules, :team_event_name
-    remove_column :schedules, :is_tournament
-    remove_column :schedules, :tournament_id
+    #remove_column :schedules, :team_event_name
+    #remove_column :schedules, :is_tournament
+    #remove_column :schedules, :tournament_id
   end
 
   def self.down
-    add_column :schedules, :is_league_run, :boolean
-    add_column :schedules, :team_event_name, :string
-    add_column :schedules, :is_tournament, :boolean
-    add_column :schedules, :tournament_id, :integer
+    #add_column :schedules, :is_league_run, :boolean
+    #add_column :schedules, :team_event_name, :string
+    #add_column :schedules, :is_tournament, :boolean
+    #add_column :schedules, :tournament_id, :integer
   end
 end
