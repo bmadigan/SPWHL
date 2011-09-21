9441b3876990b15f7f9ce257b1137afb20641fa9
Brad Madigan <brad.madigan@dizzytree.com>
2011-09-21 10:18:04 -0400

    Added proper association for league directors

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,5 +1,7 @@
 class League < ActiveRecord::Base
   belongs_to :season
+  belongs_to :director, :class_name => "User", :foreign_key => "director_id"
+  
   has_many :teams
   
   attr_accessible :name, :season_id, :is_house_league, :director_id, :page_content, :short_name
