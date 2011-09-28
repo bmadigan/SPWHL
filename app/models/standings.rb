bc9d27986a8376e0192f44959ce994e0c0d7ce0d
Brad Madigan <brad.madigan@dizzytree.com>
2011-09-28 10:05:55 -0400

    Get rid of the deleted files

--- app/models/standings.rb
+++
@@ -1,7 +0,0 @@
-class Standings < ActiveRecord::Base
-  belongs_to :league
-  belongs_to :hometeam_score, :foreign_key => 'home_team_score', :class_name => 'Team'
-  belongs_to :awayteam_score, :foreign_key => 'away_team_score', :class_name => 'Team'
-  
-  attr_accessible :league_id, :game_id, :home_team_score, :away_team_score
-end
--- app/views/home/calendar.html.rb
+++
@@ -1,9 +0,0 @@
-<% title "SPWHL - Calendar" %>
-
-<div class="grid_12">
-
-  <div style="margin:20px 0;">
-    <iframe style="border-right-width: 0px; border-right-style: initial; border-right-color: initial; border-top-width: 0px; border-top-style: initial; border-top-color: initial; border-left-width: 0px; border-left-style: initial; border-left-color: initial; border-bottom-width: 0px; border-bottom-style: initial; border-bottom-color: initial; " src="http://www.google.com/calendar/embed?src=judy.cruise%40shaw.ca&ctz=America/Toronto" frameborder="0" width="800" scrolling="no" height="600"></iframe>
-  </div>
-
-</div>
